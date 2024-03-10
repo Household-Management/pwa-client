0c58b825b65f7efffd170bd199f3d4e4c8775664
Damien Marble <damienmarble@gmail.com>
2024-03-10 10:28:36 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {BottomNavigation, BottomNavigationAction, Button, Drawer, Paper} from "@mui/material";
+import ListIcon from '@mui/icons-material/List';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import Tasks from "./tasks/components/Tasks";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Tasks/>
+    }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: "flex", flexDirection: "column"}}>
+        <RouterProvider router={router}/>
+        {/*<Paper sx={{ display:"flex", flexGrow: 0, flexShrink: 0, justifyContent: "center", bottom: 0, left: 0, right: 0 }} elevation={3}>*/}
+        {/*    <BottomNavigation showLabels={true}>*/}
+        {/*        <BottomNavigationAction label="Todos & Chores" icon={<ListIcon/>}/>*/}
+        {/*    </BottomNavigation>*/}
+        {/*</Paper>*/}
     </div>
   );
 }
