617e7252d74e13304681b58e7d6cb9a2cb06afee
Damien Marble <damienmarble@gmail.com>
2024-04-26 14:42:17 -0700

    Add reducers for deleting and updating tasks and lists.

--- src/tasks/state/TaskStateConfiguration.js
+++ src/tasks/state/TaskStateConfiguration.js
@@ -25,6 +25,32 @@ export default function (initialState) {
                 state.taskLists[action.payload.targetList].tasks.push({...action.payload.newTask});
                 return state;
             },
+            UpdateList: (state, action) => {
+                state.taskLists[action.payload.id] = action.payload;
+                return state;
+            },
+            UpdateTask: (state, action) => {
+                const task = action.payload;
+                const list = state.taskLists[state.selectedList];
+                const taskIndex = list.tasks.findIndex(t => t.id === task.id);
+                list.tasks[taskIndex] = task;
+                return state;
+            },
+            DeleteList: (state, action) => {
+                delete state.taskLists[action.payload];
+                if(action.payload === state.selectedList) {
+                    state.selectedList = Object.keys(state.taskLists)[0];
+                }
+                return state;
+            },
+            DeleteTask: (state, action) => {
+                const list = state.taskLists[action.payload.fromList];
+                list.tasks = list.tasks.filter(t => t.id !== action.payload.taskId);
+                if(action.payload.taskId === state.selectedTask) {
+                    state.selectedTask = null;
+                }
+                return state;
+            }
         }
     });
     return slice;
