b475a4c97a92a279a8cf1223ea706afaff26864f
Damien Marble <damienmarble@gmail.com>
2024-04-28 17:22:49 -0700

    Implement completion of tutorials from clicks.

--- src/tutorials/components/TutorialPopper.js
+++ src/tutorials/components/TutorialPopper.js
@@ -1,21 +1,49 @@
 import {Box, Paper, Popper} from "@mui/material";
-import React, {Fragment, useRef, useMemo} from "react";
+import React, {Fragment, useRef, useMemo, useEffect} from "react";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {getActions} from "../state/TutorialStateConfiguration";
 
-export function TutorialPopper({ targetElementSelectors, open, sx, children, highlightTarget }) {
+export function TutorialPopper({
+                                   activeTutorial,
+                                   sx,
+                                   children,
+                                   dispatch
+                               }) {
+    const matcher = useRef(null)
+    const {targetElements: targetElementSelectors, highlightTarget = true, completionMatcher} = activeTutorial || {};
+    matcher.current = completionMatcher;
     const lastTargets = useRef(null);
-    let targetElements = useMemo(() => {;
+    let targetElements = useMemo(() => {
+        ;
         const selector = !targetElementSelectors || typeof targetElementSelectors === "string" ? targetElementSelectors : targetElementSelectors.join(", ");
         // FIXME: Make resilient for bad selectors, as querySelector can throw.
-        if(typeof(targetElementSelectors) === "string") {
+        if (typeof (targetElementSelectors) === "string") {
             return document.querySelectorAll(selector);
-        } else if(Array.isArray(targetElementSelectors)) {
+        } else if (Array.isArray(targetElementSelectors)) {
             return document.querySelectorAll(selector);
         }
-    }, [targetElementSelectors, open]);
+    }, [targetElementSelectors, activeTutorial != null]);
 
-    if(open && targetElements) {
-        if(lastTargets.current) {
+    const tutorialForMatching = useRef(null);
+    // Use to make the tutorial available inside the click handler.
+    tutorialForMatching.current = activeTutorial;
+
+    useEffect(() => {
+        // FIXME: Unsubscribe when called multiple times or remove need of multiple calls.
+        targetElements?.forEach(el => {
+            el.addEventListener("click", ev => {
+                if (matcher.current && matcher.current.type === "click") {
+                    if (ev.target.matches(matcher.current.target)) {
+                        dispatch(getActions().CompleteTutorial());
+                    }
+                }
+            });
+        });
+    }, [targetElementSelectors, activeTutorial != null]);
+
+    if (activeTutorial !== null && targetElements) {
+        if (lastTargets.current) {
             lastTargets.current.forEach(el => el.style.zIndex = 0);
         }
         targetElements.forEach(el => el.style.zIndex = highlightTarget ? 100 : 0);
@@ -33,15 +61,25 @@ export function TutorialPopper({ targetElementSelectors, open, sx, children, hig
             left: 0,
             right: 0,
             bottom: 0,
-            visibility: open ? "visible" : "hidden",
+            visibility: activeTutorial != undefined ? "visible" : "hidden",
             ...sx
         }}/>
-        <Popper style={{zIndex: 1}} anchorEl={targetElements && targetElements[0]} open={open}>
-            {children}
+        <Popper style={{zIndex: 1}} anchorEl={targetElements && targetElements[0]} open={activeTutorial != undefined}>
+            {children || <Paper sx={{padding: 1}}>
+                {activeTutorial?.message}
+            </Paper>}
         </Popper>
     </Fragment>
 }
 
+export const ConnectedTutorialPopper = connect((state, ownProps) => {
+    return {
+        activeTutorial: state.tutorials.activeTutorial,
+        ...ownProps
+    }
+})(TutorialPopper);
+
+// FIXME: Correct these
 TutorialPopper.propTypes = {
     targetElementSelectors: PropTypes.arrayOf(PropTypes.string),
     open: PropTypes.bool.isRequired,
--- src/tutorials/components/TutorialTarget.stories.js
+++ src/tutorials/components/TutorialTarget.stories.js
@@ -1,74 +1,110 @@
 import React, {Fragment, useState, useRef} from "react";
-import {TutorialPopper} from "./TutorialPopper";
+import {
+    TutorialPopper as TutorialPopperComponent,
+    ConnectedTutorialPopper as Tutorials
+} from "./TutorialPopper";
 import {Paper} from "@mui/material";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import Tutorial from "../model/Tutorial";
+import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+import {TutorialStateConfiguration, getActions} from "../state/TutorialStateConfiguration";
+import * as _ from "lodash";
 
-export default {
-    args: {
-        highlightTarget: true,
-        popupMessage: "Hello World"
+export default {}
+
+const MockStore = initialState => configureStore({
+    reducer: {
+        tutorials: TutorialStateConfiguration(initialState).reducer
     }
-}
+});
 
-export const TutorialTarget = {
+export const TutorialPopper = {
+    args: {
+        tutorials: {
+            "tutorial-1": new Tutorial("tutorial-1", "This is tutorial 1", ["#tutorial-button-1"], {
+                type: "click",
+                target: "#tutorial-button-1"
+            }),
+            "tutorial-2": new Tutorial("tutorial-2", "This is tutorial 2", ["#tutorial-button-2"], {
+                type: "click",
+                target: "#tutorial-button-2"
+            }),
+            "tutorial-3": new Tutorial("tutorial-3", "This is tutorial 3", ["#tutorial-button-3"], {
+                type: "click",
+                target: "#tutorial-button-3"
+            }),
+        }
+    },
     render: (args) => {
-        const [target, setTarget] = useState(null);
         const [activeTutorial, setActiveTutorial] = useState(null);
-        const changeActiveTutorial = (id) => {
-            setActiveTutorial(id);
+        const changeActiveTutorial = (tutorial) => {
+            setActiveTutorial(tutorial);
         }
 
         return <Fragment>
-            <TutorialPopper open={activeTutorial !== null} targetElementSelector={target} highlightTarget={args.highlightTarget} >
+            <TutorialPopperComponent activeTutorial={activeTutorial}>
                 <Paper sx={{padding: 1}}>
-                    {args.popupMessage}
+                    {activeTutorial?.message}
                 </Paper>
-            </TutorialPopper>
-            <button id="tutorial-button-1" onClick={ev => {
-                changeActiveTutorial(activeTutorial === 1 ? null : 1);
-                setTarget("#" + ev.target.id);
-            }} style={{position: "relative"}}>{activeTutorial !== 1 ? "Start Tutorial 1" : "End Tutorial 1"}</button>
-            <button id="tutorial-button-2" onClick={ev => {
-                changeActiveTutorial(activeTutorial === 2 ? null : 2);
-                setTarget("#" + ev.target.id);
-            }} style={{position: "relative"}}>{activeTutorial !== 2 ? "Start Tutorial 2" : "End Tutorial 2"}</button>
-            <button id="tutorial-button-3" onClick={ev => {
-                changeActiveTutorial(activeTutorial === 3 ? null : 3)
-                setTarget("#" + ev.target.id);
-            }} style={{position: "relative"}}>{activeTutorial !== 3 ? "Start Tutorial 3" : "End Tutorial 3"}</button>
+            </TutorialPopperComponent>
+            {Object.values(args.tutorials).map(t => {
+                const tut = t;
+                return <button id={t.completionMatcher.target.substring(1)} onClick={ev => {
+                    changeActiveTutorial(activeTutorial === tut ? null : tut);
+                }}
+                               style={{position: "relative"}}>{activeTutorial !== tut ? `Start Tutorial ${tut.id}` : `End Tutorial ${tut.id}`}</button>
+            })}
         </Fragment>
     }
 }
 
-export const TutorialJsxTarget = {
+export const CompleteOnButtonClick = {
+    args: {
+        tutorials: {
+            "tutorial-1": {
+                ...new Tutorial("tutorial-1",
+                    "Tutorials can be completed when something is clicked, like a button",
+                    ["#tutorial-button-1"],
+                    {
+                        type: "navigate"
+                    },
+                    {
+                        type: "click",
+                        target: "#tutorial-button-1"
+                    })
+            },
+            "tutorial-2": {
+                ...new Tutorial("tutorial-2", "Tutorials can be completed when something is entered into an input", ["#tutorial-button-2"], {
+                    type: "change",
+                    target: "#tutorial-input-2"
+                })
+            },
+        },
+    },
+    decorators: [(story, opts) => {
+        const store = MockStore({
+            tutorials: opts.args.tutorials,
+            activeTutorial: null
+        });
+        opts.args.getState = () => store.getState();
+        opts.args.dispatch = store.dispatch;
+        return <Provider store={store}>{story()}</Provider>
+    }],
     render: (args) => {
-        const [target, setTarget] = useState(null);
-        const [activeTutorial, setActiveTutorial] = useState(null);
-        const paper = useRef(null);
-        const changeActiveTutorial = (id) => {
-            setActiveTutorial(id);
-        }
-
         return <Fragment>
-            <TutorialPopper open={activeTutorial !== null} targetElement={target} highlightTarget={args.highlightTarget}>
-                <Paper sx={{padding: 1}}>
-                    {args.popupMessage}
-                </Paper>
-            </TutorialPopper>
-            <button onClick={ev => {
-                changeActiveTutorial(activeTutorial === 1 ? null : 1);
-                setTarget(paper.current);
-            }} style={{position: "relative"}}>{activeTutorial !== 1 ? "Start Tutorial 1" : "End Tutorial 1"}</button>
-            <Accordion ref={paper} style={{position: "relative"}}>
-                <AccordionSummary>
-                    Summary
-                </AccordionSummary>
-                <AccordionDetails>
-                    Body
-                </AccordionDetails>
-            </Accordion>
+            <Tutorials>
+
+            </Tutorials>
+            <button id="tutorial-button-1" onClick={ev => {
+                if (!args.getState().tutorials.activeTutorial) {
+                    args.dispatch(getActions().StartTutorial("tutorial-1"))
+                }
+            }}
+                    style={{position: "relative"}}>{args.getState().tutorials.activeTutorial !== 1 ? "Start Tutorial 1" : "End Tutorial 1"}</button>
+            <input id="tutorial-input-1"/>
         </Fragment>
     }
 }
