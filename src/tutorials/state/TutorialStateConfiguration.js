38d824d49574eebd5965d8ed18a0e756d745aec7
Damien Marble <damienmarble@gmail.com>
2024-04-28 17:22:25 -0700

    Add reducers and modify initial state.

--- src/tutorials/state/TutorialStateConfiguration.js
+++ src/tutorials/state/TutorialStateConfiguration.js
@@ -1,18 +1,42 @@
 import {createSlice} from "@reduxjs/toolkit";
+import Tutorial from "../model/Tutorial";
+
 let slice = undefined;
+
+// TODO: Validate initial state
 export function TutorialStateConfiguration(initialState) {
     slice = createSlice({
         name: "tasks",
         initialState: initialState ? initialState : {
-            tutorials: {}
+            tutorials: {
+                "create-list": {
+                    ...new Tutorial("create-list", "#To create new lists of tasks, click here", ["#create-list"],
+                        {type: "click", target: "#create-list"})
+                },
+            }
         },
         reducers: {
-
+            StartTutorial(state, action) {
+                if (!state.tutorials[action.payload].completed && !state.activeTutorial) {
+                    state.activeTutorial = {...state.tutorials[action.payload]};
+                }
+                return state;
+            },
+            CompleteTutorial(state, action) {
+                state.tutorials[state.activeTutorial.id].completed = true;
+                state.activeTutorial = undefined;
+                return state;
+            },
+            ReadyTutorial(state, action) {
+                state.activeTutorial.ready = true;
+                return state;
+            }
         }
     });
     return slice;
 }
 
+// TODO: Make property of config function?
 export function getActions() {
     return slice.actions;
 }
