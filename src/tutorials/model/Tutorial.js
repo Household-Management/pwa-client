1d8780b979bbc5169a646f48805f9ac4a9e4196d
Damien Marble <damienmarble@gmail.com>
2024-04-28 17:22:06 -0700

    Reorganize model.

--- src/tutorials/model/Tutorial.js
+++ src/tutorials/model/Tutorial.js
@@ -2,8 +2,28 @@
  * A user tutorial.
  */
 export default class Tutorial {
-    constructor(id, message) {
+    id
+    message
+    targetElements
+    startMatcher
+    completionMatcher
+    willHighlightTargets
+    /**
+     *  @param {string} id - The unique identifier for the tutorial.
+     *  @param {string} message - The message to display to the user.
+     *  @param {string[]} targetElements - The elements to highlight.
+     *  @param {object} startMatcher - The matcher for the conditions which will start the tutorial.
+     *  @param {object} completionMatcher - The matcher for the conditions which will complete the tutorial.
+     *  @param {boolean} highlightTarget - Whether to darken the ui to highlight the target elements.
+     */
+    constructor(id, message, targetElements, startMatcher, completionMatcher, highlightTarget = true) {
         this.id = id;
         this.message = message;
+        this.targetElements = targetElements;
+        // TODO : Validate completion matcher
+        this.startMatcher = startMatcher;
+        this.willHighlightTargets = highlightTarget;
+        // TODO : Validate completion matcher
+        this.completionMatcher = completionMatcher;
     }
 }
