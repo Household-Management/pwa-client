c3657b467182153f8e2c9c8b3be61023ddc241f2
ThisIsNoZaku <damienmarble@gmail.com>
2025-02-25 09:43:38 -0800

    Make the top bar toggleable to show which subview is active.

--- src/kitchen/components/KitchenView.js
+++ src/kitchen/components/KitchenView.js
@@ -1,38 +1,45 @@
-import {Link, NavLink, Outlet} from "react-router-dom";
-import { useHeader } from "../../layout/hooks/HeaderContext";
+import {NavLink, Outlet} from "react-router-dom";
+import {useHeader} from "../../layout/hooks/HeaderContext";
 import React, {useEffect} from "react";
-import {BottomNavigationAction, Button, Toolbar} from "@mui/material"
+import {Button, ToggleButton, ToggleButtonGroup, Toolbar} from "@mui/material"
 
 const KitchenView = () => {
-    const { setHeaderContent } = useHeader();
-
+    const {setHeaderContent} = useHeader();
+    const [selectedView, setSelectedView] = React.useState(window.location.pathname);
     // Set the dynamic header content
     useEffect(() => {
         setHeaderContent(
-            <Toolbar sx={{ width: "100%", "justify-content" : "center", boxSizing : "border-box"}}>
-                <NavLink to="/kitchen/pantry" className="nav-link">
-                    <Button>
-                        Pantry & Fridge
-                    </Button>
-                </NavLink>
-                <NavLink to="/kitchen/recipes" className="nav-link">
-                    <Button>
-                        Recipes
-                    </Button>
-                </NavLink>
-                <NavLink to="/kitchen/grocery" className="nav-link">
-                    <Button>
-                        Groceries
-                    </Button>
-                </NavLink>
+            <Toolbar sx={{width: "100%", "justify-content": "center", boxSizing: "border-box"}}>
+                <ToggleButtonGroup
+                    color="primary"
+                    value={window.location.pathname}
+                    exclusive
+                    onChange={(event, value) => setSelectedView(value)}
+                >
+                    <NavLink to="/kitchen/pantry" className="nav-link">
+                        <ToggleButton value="/kitchen/pantry">
+                            Pantry & Fridge
+                        </ToggleButton>
+                    </NavLink>
+                    <NavLink to="/kitchen/recipes" className="nav-link">
+                        <ToggleButton value="/kitchen/recipes">
+                            Recipes
+                        </ToggleButton>
+                    </NavLink>
+                    <NavLink to="/kitchen/grocery" className="nav-link">
+                        <ToggleButton value="/kitchen/grocery">
+                            Groceries
+                        </ToggleButton>
+                    </NavLink>
+                </ToggleButtonGroup>
             </Toolbar>
         );
-    }, [setHeaderContent]);
+    }, [setHeaderContent, selectedView]);
 
 
     return (
         <div className="content">
-            <Outlet />
+            <Outlet/>
         </div>
     );
 };
