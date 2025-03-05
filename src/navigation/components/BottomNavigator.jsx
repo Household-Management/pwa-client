1c1011a474e391eca5855d9a24ff6ad5cab4d9b6
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-04 17:19:42 -0800

    Show the correct bottom button based on path.

--- src/navigation/components/BottomNavigator.jsx
+++ src/navigation/components/BottomNavigator.jsx
@@ -5,11 +5,25 @@ import KitchenIcon from "@mui/icons-material/Kitchen";
 import React, {useState} from "react";
 
 export default function BottomNavigator(props) {
-    const [tab, setTab] = useState(0)
+    const [tab, setTab] = useState(routes.findIndex(({path}) => window.location.pathname === path));
     return <Paper id="blahblah" sx={{ display:"flex", position: "relative", flexGrow: 0, flexShrink: 1, justifyContent: "center", bottom: "10px" }} elevation={3}>
-        <BottomNavigation showLabels={true} value={tab} onChange={(evt, newValue) => setTab(newValue)}>
-            <BottomNavigationAction label="Todos & Chores" icon={<ListIcon/>} component={Link} to="/tasks"  />
-            <BottomNavigationAction label="Pantry and Recipes" icon={<KitchenIcon/>} component={Link} to="/kitchen/pantry"  />
+        <BottomNavigation showLabels value={tab} onChange={(evt, newValue) => setTab(newValue)}>
+            {
+                routes.map(({label, icon, path}) => (<BottomNavigationAction key={path} label={label} icon={icon} component={Link} to={path} />))
+            }
         </BottomNavigation>
     </Paper>
 }
+
+const routes = [
+    {
+        label: "Todos & Chores",
+        icon: <ListIcon/>,
+        path: "/tasks"
+    },
+    {
+        label: "Pantry and Recipes",
+        icon: <KitchenIcon/>,
+        path: "/kitchen/pantry"
+    }
+];
