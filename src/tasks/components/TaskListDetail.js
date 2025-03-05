979466fc1373b566dcddb40a898169ff132fa443
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-04 18:29:36 -0800

    Toggle list name editing.

--- src/tasks/components/TaskListDetail.js
+++ src/tasks/components/TaskListDetail.js
@@ -1,16 +1,14 @@
 import {
-    Box,
     List,
     ListItem,
     ListItemButton, ListSubheader,
     Paper, TextField,
 } from "@mui/material";
-import {AddCircleOutline, Delete} from "@mui/icons-material";
-import {Fragment, useState} from "react";
+import {AddCircleOutline, Delete, Edit} from "@mui/icons-material";
+import {Fragment, useEffect, useRef, useState} from "react";
 import TaskDetailAccordion from "./TaskDetailAccordion";
-import {IconButton} from "@mui/material";
 import PropTypes from "prop-types";
-import IconTile from "../../IconTile";
+import Fab from "@mui/material/Fab";
 
 /**
  * Component for displaying the detailed view of a TaskList model instance.
@@ -20,14 +18,43 @@ import IconTile from "../../IconTile";
  */
 // TODO: Edit name of list
 export default function TaskListDetail({list, onTaskChanged, onTaskCreated, onTaskSelected, onListChanged, onListDelete, onTaskDelete, selectedTask}) {
+    const [editing, setEditing] = useState(false);
+    const [selectedList, setSelectedList] = useState(list.id);
+
+    const textFieldRef = useRef(null);
+
+    useEffect(() => {
+        setSelectedList(list.id);
+    }, [list]);
+
+    useEffect(() => {
+        setEditing(false);
+    }, [selectedList]);
+
+    useEffect(() => {
+        if(editing && textFieldRef.current) {
+            textFieldRef.current.focus();
+        }
+    }, [editing]);
+
     return <Fragment>
         <Paper sx={{height: "100%"}}>
             <List >
                 <ListSubheader sx={{width: "100%", display: "flex", justifyContent: "space-between"}}>
-                    <TextField sx={{flexGrow: 9}} value={list.name} onChange={ev => onListChanged({...list, name: ev.target.value})}
-                    disable={list.unremovable}/>
-                    {!list.unremovable && <div style={{flexGrow: 1, display: "flex"}}>
-                        <IconTile sx={{aspectRatio: "1/1", height: "100%"}} color="error" onClick={onListDelete.bind(null, list.id)} icon={<Delete/>}/>
+                    <TextField sx={{flexGrow: 9}}
+                               value={list.name}
+                               placeholder="New List"
+                               onChange={ev => onListChanged({...list, name: ev.target.value})}
+                               disabled={list.unremovable || !editing}
+                               inputRef={textFieldRef}
+                    />
+                    {!list.unremovable && <div style={{flexGrow: 1, display: "flex", justifyContent: "space-around"}}>
+                        <Fab color="primary" onClick={() => setEditing(!editing)}>
+                            <Edit/>
+                        </Fab>
+                        <Fab color="error" onClick={onListDelete.bind(null, list.id)} >
+                            <Delete/>
+                        </Fab>
                     </div>}
                 </ListSubheader>
                 {/* FIXME: Make a nice header */}
