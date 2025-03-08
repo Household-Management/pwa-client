0357c8c13ece83f15c1c7a0d8c79373d7097554f
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-08 11:15:26 -0800

    Clear header on settings page.

--- src/settings/components/SettingsView.jsx
+++ src/settings/components/SettingsView.jsx
@@ -2,10 +2,13 @@ import {Stack, Button} from "@mui/material";
 import {useContext} from "react";
 import {ServiceWorkerContext} from "../../service-worker/ServiceWorkerContext";
 import {useDispatch} from "react-redux";
+import {useHeader} from "../../layout/hooks/HeaderContext";
 
 export default function () {
     const wb = useContext(ServiceWorkerContext);
     const dispatch = useDispatch();
+    const { setHeaderContent} = useHeader()
+    setHeaderContent(null)
     function logOut() {
         wb.active.then(async () => {
             const response = await wb.messageSW({
