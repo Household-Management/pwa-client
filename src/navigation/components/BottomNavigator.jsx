c94f0defb66a918ae49b0ed9be216281a86f4d9a
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-08 10:21:16 -0800

    Space evenly in the bottom.

--- src/navigation/components/BottomNavigator.jsx
+++ src/navigation/components/BottomNavigator.jsx
@@ -2,12 +2,13 @@ import {BottomNavigation, BottomNavigationAction, Paper} from "@mui/material";
 import ListIcon from "@mui/icons-material/List";
 import {Link} from "react-router-dom";
 import KitchenIcon from "@mui/icons-material/Kitchen";
+import SettingsIcon from "@mui/icons-material/Settings";
 import React, {useState} from "react";
 
 export default function BottomNavigator(props) {
     const [tab, setTab] = useState(routes.findIndex(({path}) => window.location.pathname.startsWith(path)));
     return <Paper id="blahblah" sx={{ display:"flex", position: "relative", flexGrow: 0, flexShrink: 1, justifyContent: "center", bottom: "10px" }} elevation={3}>
-        <BottomNavigation showLabels value={tab} onChange={(evt, newValue) => setTab(newValue)}>
+        <BottomNavigation showLabels value={tab} onChange={(evt, newValue) => setTab(newValue)} sx={{justifyContent: "space-evenly", flexGrow: 1}}>
             {
                 routes.map(({label, icon, path}) => (<BottomNavigationAction key={path} label={label} icon={icon} component={Link} to={path} />))
             }
@@ -25,5 +26,10 @@ const routes = [
         label: "Pantry and Recipes",
         icon: <KitchenIcon/>,
         path: "/kitchen/pantry"
+    },
+    {
+        label: "Settings",
+        icon: <SettingsIcon/>,
+        path: "/settings"
     }
 ];
