e95900ef43afb3c7dddfda9add8b95ac3581e5d1
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-08 10:21:33 -0800

    Store login state in cookies.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito';
+import { CookieStorage } from 'aws-amplify/utils';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -9,3 +11,6 @@ const backend = defineBackend({
   auth,
   data,
 });
+
+
+cognitoUserPoolsTokenProvider.setKeyValueStorage(new CookieStorage());
