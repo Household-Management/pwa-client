75d8c358ae9cd712b13521b9dd460fd2cd9bca89
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-10 14:36:29 -0700

    Don't save when irrelevant actions occur.
    Load on authenticate. Use saga for handling persistence, not a store subscription.

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,9 @@ import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import TaskStateConfiguration from "../tasks/state/TaskStateConfiguration";
 import {TutorialStateConfiguration} from "../tutorials/state/TutorialStateConfiguration";
 import KitchenStateConfiguration from "../kitchen/state/KitchenStateConfiguration";
-import AlertsStateConfiguration from "../alerts/configuration/AlertsStateConfiguration";
-import {getActions as GetAlertsActions} from "../alerts/configuration/AlertsStateConfiguration";
+import AlertsStateConfiguration, {Alert} from "../alerts/configuration/AlertsStateConfiguration";
 import {ServiceWorkerContext} from "../service-worker/ServiceWorkerContext";
-import {put, takeLeading, select} from "redux-saga/effects";
+import {put, takeLeading, select, call, spawn, debounce, takeLatest} from "redux-saga/effects";
 import * as _ from "lodash";
 import createSagaMiddleware from "redux-saga";
 
@@ -14,15 +13,18 @@ import createSagaMiddleware from "redux-saga";
 // TODO: Implement user tutorials.
 // TODO: Middleware for intercepting dangerous actions.
 const combinedReducer = combineReducers({
-    tasks: TaskStateConfiguration().reducer,
+    householdTasks: TaskStateConfiguration,
     tutorials: TutorialStateConfiguration().reducer,
     kitchen: KitchenStateConfiguration(),
-    alerts: AlertsStateConfiguration(),
+    alerts: AlertsStateConfiguration,
     user: (state = null, action) => {
         if (action.type === "UNAUTHENTICATED") {
-            return null;
+            return {
+                user: null,
+                welcomed: false
+            };
         } else if (action.type === "AUTHENTICATED") {
-            return action.data;
+            return {...state, user: action.data};
         } else {
             return state ? state : {
                 user: null,
@@ -34,34 +36,53 @@ const combinedReducer = combineReducers({
 
 const saga = createSagaMiddleware()
 
-function* welcomeUser(action) {
+function* welcomeUser() {
     let welcomed = false
     yield takeLeading("AUTHENTICATED", function* (action) {
-        const user = yield select(state => state.user);
-        if(!welcomed) {
+        if (!welcomed) {
+            const user = yield select(state => state.user);
             welcomed = true
             yield put(
-                GetAlertsActions().Alert({message: `Welcome ${user.signInDetails.loginId}`})
+                Alert({message: `Welcome ${user.user.signInDetails.loginId}`})
             )
         }
+    })
+}
+
+function* loadOnAuthenticate() {
+    yield takeLeading("AUTHENTICATED", function* (action) {
+        const sw = yield call(() => ServiceWorkerContext.Provider._context._currentValue);
+        const loaded = yield call(() => sw.messageSW({
+            type: "LOAD_STATE"
+        }));
+        yield put({
+            type: "LOADED_STATE",
+            data: loaded.data,
+            noSave: true
+        });
+    })
+}
+
+function* persistOnChange() {
+    yield debounce(500, action => !action.noSave, function* (action) {
+        yield call(async () => {
+            // FIXME: This is a hack to get the service worker context.
+            const wb = ServiceWorkerContext.Provider._context._currentValue;
+            const state = store.getState()
+            return await wb.messageSW({
+                type: 'SAVE_STATE',
+                noSave: true,
+                state: JSON.stringify(_.omit(state, ["alerts", "user", "tutorials"])) // TODO: Save tutorials attached to users, not households.
+            })
+        })
     })
 }
 
 export const store = configureStore({
     reducer: (state, action) => {
         switch (action.type) {
-            case "WELCOME":
-                return {
-                    ...state, user: {
-                        ...state.user,
-                        welcomed: true
-                    }
-                }
-            case "AUTHENTICATED":
-                return {...state, user: action.data};
-            case "UNAUTHENTICATED":
-                return {...state, user: null};
-            case "LOAD_STATE":
+            case "LOADED_STATE":
+                console.log("Setting loaded state");
                 if (action.data) {
                     return {...state, ...JSON.parse(action.data)};
                 }
@@ -74,18 +95,22 @@ export const store = configureStore({
     }
 });
 
-saga.run(welcomeUser)
-
-// TODO: On load, show notification of tasks that are due today.
-store.subscribe(() => {
-    // TODO: Save is being triggered multiple times, move into saga to prevent multiple saves.
-    console.log("Persisting state after store update");
-    const wb = ServiceWorkerContext.Provider._context._currentValue;
-    wb.active.then(x => {
-        const state = store.getState()
-        console.log("save", state.alerts.active)
-        wb.messageSW({
-            type: 'SAVE_STATE', state: JSON.stringify(_.omit(state, ["alerts"]))
-        })
-    });
+saga.run(function*() {
+    yield spawn(welcomeUser)
+    yield spawn(loadOnAuthenticate)
+    yield spawn(persistOnChange)
 });
+
+// // TODO: On load, show notification of tasks that are due today.
+// store.subscribe(() => {
+//     // TODO: Save is being triggered multiple times, move into saga to prevent multiple saves.
+//     console.log("Persisting state after store update");
+//     const wb = ServiceWorkerContext.Provider._context._currentValue;
+//     wb.active.then(x => {
+//         console.log("Web worker active, sending state");
+//         const state = store.getState()
+//         wb.messageSW({
+//             type: 'SAVE_STATE', state: JSON.stringify(_.omit(state, ["alerts", "user", "tutorials"])) // TODO: Save tutorials attached to users, not households.
+//         })
+//     });
+// });
