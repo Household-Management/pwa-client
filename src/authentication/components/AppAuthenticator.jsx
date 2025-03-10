4529c09c25f3e4bbb0fb590ac5dba0c121ab3046
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-10 14:38:51 -0700

    Added logic for determining when to show login modal vs loader vs content.

--- src/authentication/components/AppAuthenticator.jsx
+++ src/authentication/components/AppAuthenticator.jsx
@@ -1,52 +1,48 @@
-import {getCurrentUser} from "@aws-amplify/auth";
 import '@aws-amplify/ui-react/styles.css';
 import {useContext, useEffect, useState} from "react";
-import {Hub} from "@aws-amplify/core";
 import SignUp from "./SignUp";
-import {Box, Modal, Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
+import {Box, CircularProgress, Modal, Stack, ToggleButton, ToggleButtonGroup} from "@mui/material";
 import SignIn from "./SignIn";
 import {useDispatch, useSelector} from "react-redux";
 import {ServiceWorkerContext} from "../../service-worker/ServiceWorkerContext";
 
-// FIXME: Sign in modal shows briefly on load, show a loader instead.
 export default function AppAuthenticator({children}) {
     const wb = useContext(ServiceWorkerContext);
     const dispatch = useDispatch();
+    const [authenticationNeeded, setAuthenticationNeeded] = useState(false);
+    const user = useSelector(state => state.user.user);
 
     useEffect(() => {
-        wb.active.then(() => {
-            wb.messageSW({
-                type: 'AUTHENTICATE',
-                payload: {
-                    useExisting: true
+        if (user) {
+            setAuthenticationNeeded(false)
+        }
+        if (!user && !authenticationNeeded) {
+            wb.active.then(async () => {
+                const response = await wb.messageSW({
+                    type: 'AUTHENTICATE',
+                    payload: {
+                        useExisting: true
+                    }
+                });
+                if (response.error) {
+                    setAuthenticationNeeded(true);
+                    throw new Error("Error in service worker: " + response.error.message);
                 }
-            }).then(data => {
+                setAuthenticationNeeded(false);
                 dispatch({
                     type: "AUTHENTICATED",
-                    data: data.payload
-                })
+                    data: response.payload,
+                    noSave: true
+                });
             });
-
-            // wb.messageSW({
-            //     type: "LOAD_STATE"
-            // }).then((data) => {
-            //     console.log("Loaded from service worker.");
-            //     store.dispatch({
-            //         type: "LOAD_STATE", data
-            //     });
-            //     store.dispatch(getAlertActions().Alert({message: "State loaded", type: "info"}));
-            // }, err => {
-            //     console.error(err);
-            // });
-        });
-
-    }, [wb]);
-
-    const user = useSelector(state => state.user);
-    useEffect(() => {
-    }, []);
+        }
+    }, [user, authenticationNeeded]);
     return <>
-        {user ? children : <Authentication/>}
+        {user ? children : (authenticationNeeded ? <Authentication/> : (
+            <Box sx={{width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <CircularProgress/>
+            </Box>))
+        }
     </>
 }
 
@@ -67,13 +63,13 @@ function Authentication() {
     return <Modal open={true}>
         <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100vw"}}>
             <Box sx={modalStyle}>
-                <Stack sx={{flexGrow: 1, justifyContent: "center", alignContent: "center"}}>
-                    <Stack direction="row" sx={{justifyContent: "center", alignContent: "center"}}>
-                        <ToggleButtonGroup exclusive={true} value={tab} onChange={(e, v) => setTab(v)}>
+                <Stack sx={{flexGrow: 1, justifyContent: "center", alignContent: "center"}} spacing={3}>
+                    <ToggleButtonGroup sx={{justifyContent: "center", alignContent: "center"}} exclusive={true} value={tab} onChange={(e, v) => setTab(v)}>
+                    <Stack direction="row"  spacing={1}>
                             <ToggleButton value={0}>Sign In</ToggleButton>
                             <ToggleButton value={1}>Sign Up</ToggleButton>
-                        </ToggleButtonGroup>
                     </Stack>
+                    </ToggleButtonGroup>
                     <div>
                         {tab === 0 && <SignIn/>}
                         {tab === 1 && <SignUp/>}
