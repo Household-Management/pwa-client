30b20cd14965e8bc6180ae38fdcc1ae9791b08de
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-11 11:39:00 -0700

    Move service worker data script.

--- src/service-worker/data/data.js
+++ src/service-worker/data/data.js
@@ -1,10 +1,23 @@
 import {getCurrentUser} from "aws-amplify/auth";
+import {fetchData, pushData} from "./remote";
+import {loadData, saveData} from "./local";
 
-export async function loadData(event) {
+export async function getData(event) {
+    // Try to load from the remote server. If that fails, try to load from indexeddb.
     const currentUser = await getCurrentUser();
-    dataClient.models.Kitchen.get({
-        owner: currentUser.userId
-    })
+    let data;
+    try {
+        // TODO: Move into background sync to refresh local data
+        // data = await fetchData(currentUser);
+    } catch (e) {
+        console.error("Failed to load data from remote server", e);
+    }
+
+    if (!data) {
+        data = await loadData();
+    }
+
+    return data;
     /*
     loadData().then(data => {
         console.log("Loaded data")
@@ -73,75 +86,10 @@ export async function loadData(event) {
 */
 }
 
-export async function saveData(event) {
-    console.log("Doing save")
-    const {payload} = event.data;
-    return dbOpen().then(db => {
-        console.log("Starting save transaction")
-        const tx = db.transaction("state", "readwrite");
-        tx.oncomplete = (() => {
-            console.log("Save transaction done")
-        });
-        tx.onabort = (() => {
-            console.log("Save transaction aborted")
-        });
-        tx.onerror = (() => {
-            console.log("Save transaction error")
-        });
-        const store = tx.objectStore("state");
-        console.log("Putting into db");
-        store.put({id: 1, state: payload});
-    }, err => console.error(err.message));
+export async function putData(event) {
+    const currentUser = await getCurrentUser();
+    const payload = JSON.parse(event.data.state);
+
+    await saveData(currentUser, payload);
 }
 
-// function loadData() {
-//     return dbOpen().then(async (db) => {
-//         console.log("Starting load transaction")
-//         const tx = db.transaction("state", "readonly");
-//         tx.oncomplete = (() => {
-//             console.log("Load transaction done")
-//         });
-//         tx.onabort = (() => {
-//             console.log("Load transaction aborted")
-//         });
-//         tx.onerror = (() => {
-//             console.log("Load transaction error")
-//         });
-//         console.log("Reading from db");
-//         const store = tx.objectStore("state");
-//         const out = await new Promise((resolve, reject) => {
-//             const req = store.get(1);
-//             req.onsuccess = ev => {
-//                 console.log("Read data from db");
-//                 resolve(ev.target.result?.state);
-//             }
-//             req.onerror = ev => reject(ev);
-//         });
-//         return out;
-//     }, (err) => {
-//         console.error(err);
-//     });
-// }
-
-function dbOpen() {
-    console.log("Opening db...")
-    return new Promise((resolve, reject) => {
-
-        const connection = indexedDB.open("state", 1);
-        connection.onsuccess = ev => {
-            console.log("Opened database");
-            resolve(ev.target.result);
-        }
-        connection.onerror = ev => reject(ev);
-
-        connection.onupgradeneeded = ev => {
-            console.log("Upgrading database");
-            const db = ev.target.result;
-            const objectStore = db.createObjectStore("state", {keyPath: "id"});
-
-            objectStore.transaction.oncomplete = ev => {
-            }
-        }
-    });
-
-}
