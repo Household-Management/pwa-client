43536bc1a72a313931f9af55ed15036d0f997259
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-16 12:19:16 -0700

    Try changing the build output based on environment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {VitePWA} from "vite-plugin-pwa";
-// @ts-ignore
-import config from "./workbox-config";
-import {injectManifest} from "workbox-build";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '/',
-    plugins: [
-        react(),
-        VitePWA({
-            injectManifest: {
-                injectionPoint: undefined,
-                globDirectory: './dist',
-                globPatterns: ['**/*.{js,css,html,png,svg}'],
-            },
-            srcDir: "src",
-            outDir: "public",
-            strategies: "injectManifest",
-            filename: "service-worker.js",
-            registerType: "autoUpdate"
-        })
-    ],
-    build: {
-        sourcemap: true
-    }
-})
+export default defineConfig(({mode}) => {
+    console.log(mode)
+    return ({
+        base: '/',
+        plugins: [
+            react(),
+            VitePWA({
+                injectManifest: {
+                    injectionPoint: undefined,
+                    globDirectory: './dist',
+                    globPatterns: ['**/*.{js,css,html,png,svg}'],
+                },
+                srcDir: "src",
+                outDir: mode === "production" ? "dist" : "public",
+                strategies: "injectManifest",
+                filename: "service-worker.js",
+                registerType: "autoUpdate"
+            })
+        ],
+        build: {
+            sourcemap: true
+        }
+    })
+});
