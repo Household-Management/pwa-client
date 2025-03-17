a2a283de609036246ab668afb5b9299dc706870e
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-17 13:03:43 -0700

    Add remote data operations.

--- src/service-worker/data/data.js
+++ src/service-worker/data/data.js
@@ -1,94 +1,36 @@
 import {getCurrentUser} from "aws-amplify/auth";
 import {loadData, saveData} from "./local";
+import {fetchData, pushData} from "./remote";
 
-export async function getData(event) {
-    // Try to load from the remote server. If that fails, try to load from indexeddb.
+
+export async function getData(event, options) {
     const currentUser = await getCurrentUser();
     let data;
+    if (!options?.remoteOnly) {
+        data = await loadData();
+    }
+
     try {
-        // TODO: Move into background sync to refresh local data
-        // data = await fetchData(currentUser);
+        if(!options?.localOnly && !data) {
+            data = await fetchData(currentUser);
+        }
     } catch (e) {
         console.error("Failed to load data from remote server", e);
     }
 
-    if (!data) {
-        data = await loadData();
-    }
-
     return data;
-    /*
-    loadData().then(data => {
-        console.log("Loaded data")
-        event.ports[0].postMessage(data);
-        if (true) {
-            data = JSON.parse(data);
-            let numberTasksDue = 0;
-            for (const list of Object.values(data?.tasks?.taskLists || {})) {
-                numberTasksDue += list.tasks.length;
-            }
-
-            const expiredItems = (data?.kitchen?.pantry?.items || []).filter(item => {
-                const remainingTime = (item.expiration ? moment(item.expiration).diff(moment(), "days") : 9999);
-                return remainingTime < 0;
-            }).length;
-            const expiringItems = (data?.kitchen?.pantry?.items || []).filter(item => {
-                const remainingTime = (item.expiration ? moment(item.expiration).diff(moment(), "days") : 9999);
-                return remainingTime <= 3 && remainingTime >= 0;
-            }).length;
-
-            console.log("Sending notifications")
-            self.clients.matchAll({
-                includeUncontrolled: true
-            }).then((clients) => {
-                console.log("Notifying " + clients.length + " clients");
-                if (numberTasksDue > 0) {
-                    // eslint-disable-next-line no-undef
-                    clients.forEach(client => {
-                        client.postMessage({
-                            type: "ALERT",
-                            payload: JSON.stringify({
-                                message: `${numberTasksDue} task(s) due today.`,
-                                type: "warning"
-                            }),
-                        })
-                    });
-                }
-
-                if (expiredItems > 0) {
-                    clients.forEach(client => {
-                        client.postMessage({
-                            type: "ALERT",
-                            payload: JSON.stringify({
-                                message: `${expiredItems} pantry items(s) have expired!`,
-                                type: "error"
-                            }),
-                        })
-                    });
-                }
-
-                if (expiringItems > 0) {
-                    // eslint-disable-next-line no-undef
-                    clients.forEach(client => {
-                        client.postMessage({
-                            type: "ALERT",
-                            payload: JSON.stringify({
-                                message: `${expiringItems} pantry items(s) expiring soon.`,
-                                type: "warning"
-                            }),
-                        });
-                    })
-                }
-            });
-        }
-    }, err => console.error(err));
-*/
 }
 
-export async function putData(event) {
+export async function putData(event, options) {
     const currentUser = await getCurrentUser();
     const payload = JSON.parse(event.data.state);
 
-    await saveData(currentUser, payload);
+    if(!options?.remoteOnly) {
+        await saveData(currentUser, payload);
+    }
+
+    if (!options?.localOnly) { {
+        await pushData(currentUser, payload);}
+    }
 }
 
