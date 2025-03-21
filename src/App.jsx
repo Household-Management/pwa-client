82b54ec6a270afbcb038dded8784d664d00fd149
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-20 22:43:45 -0700

    Add cookies support.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import React, {useContext} from "react";
 import {Amplify} from "aws-amplify";
 import outputs from "../amplify_outputs";
 import {store} from "./redux/store";
-import AppAuthenticator from "./authentication/components/AppAuthenticator";
 import {HeaderProvider} from "./layout/hooks/HeaderContext";
 import {RouterProvider} from "react-router";
 import {router} from "./navigation/configuration/routing";
+import {DataClientContext} from "./graphql/DataClient";
+import {CookiesProvider} from "react-cookie";
 
 console.log("Loading state from window");
 
@@ -18,9 +19,11 @@ Amplify.configure(outputs);
 function App() {
     return (<div className="App" style={{display: "flex", flexDirection: "column", height: "100vh"}}>
         <Provider store={store}>
-                <HeaderProvider>
+            <HeaderProvider>
+                <CookiesProvider>
                     <RouterProvider router={router}/>
-                </HeaderProvider>
+                </CookiesProvider>
+            </HeaderProvider>
         </Provider>
     </div>);
 
