3a3f1a6094627010f5bc8772b5365f2b983ce062
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-20 22:48:48 -0700

    Don't defer to service worker for sign out and clear household cookie.

--- src/settings/components/SettingsView.jsx
+++ src/settings/components/SettingsView.jsx
@@ -4,21 +4,28 @@ import {useContext, useEffect} from "react";
 import {ServiceWorkerContext} from "../../service-worker/ServiceWorkerContext";
 import {useDispatch} from "react-redux";
 import {useHeader} from "../../layout/hooks/HeaderContext";
+import {signOut} from "@aws-amplify/auth";
+import {useCookies} from "react-cookie";
 
 export default function () {
     const wb = useContext(ServiceWorkerContext);
     const dispatch = useDispatch();
     const { setHeaderContent} = useHeader()
+    const [cookies, setCookie] = useCookies();
     useEffect(() => {
         setHeaderContent(null)
     }, []);
     function logOut() {
         wb.active.then(async () => {
-            const response = await wb.messageSW({
-                type: "SIGN_OUT"
+            await signOut();
+            setCookie('household', null);
+            // const response = await wb.messageSW({
+            //     type: "SIGN_OUT"
+            // });
+            // console.log("Received sign out response");
+            dispatch({
+                type: "UNAUTHENTICATED"
             });
-            console.log("Received sign out response");
-            dispatch(response);
         });
     }
     return <Stack spacing={2}>
