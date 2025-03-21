f38d62e2e033d012b5967a56acb8c8d0db4bf065
ThisIsNoZaku <damienmarble@gmail.com>
2025-03-20 22:52:20 -0700

    Fixed the relationships between model objects.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,67 +1,89 @@
 import {type ClientSchema, a, defineData} from '@aws-amplify/backend';
-import {SchemaModelGenerator} from "../../src/tasks/model/Task"
 
-const schema = a.schema({
-    Household: a.model({
-        id: a.id().required(),
-        kitchen: a.hasOne("Kitchen", "id"),
-        householdTasks: a.hasOne("HouseholdTasks", "id"),
-        recipes: a.hasOne("HouseholdRecipes", "id"),
-        members: a.string().array(),
-    }),
+function defaultOperations(allow) {
+    return [
+        allow.ownersDefinedIn("owners").to(["read", "update", "create"]),
+    ]
+}
+
+function ownedModel(modelDef) {
+    return {
+        ...modelDef,
+        membersGroup: a.string().required(), // Name for group members of the household
+        adminGroup: a.string().required(), // Name for admins of the household
+    }
+}
+
+const tasksModels = {
     HouseholdTasks: a.model({
         id: a.id().required(),
-        taskLists: a.hasMany("TaskList", "id"),
-        household: a.belongsTo("Household", "id"),
+        taskLists: a.hasMany("TaskList", "householdTasksId"),
+        householdId: a.id(),
+        household: a.belongsTo("Household", "householdId"),
     }),
     TaskList: a.model({
         id: a.id().required(),
         name: a.string(),
-        taskItems: a.hasMany("Task", "id"),
-        listTasks: a.belongsTo("HouseholdTasks", "id"),
+        householdTasksId: a.id(),
+        householdTasks: a.belongsTo("HouseholdTasks", "householdTasksId"),
+        taskItems: a.hasMany("Task", "taskListId"),
     }),
     Task: a.model({
         id: a.id().required(),
         title: a.string(),
         scheduledTime: a.string(),
-        repeats: a.hasOne("TaskRepeat", "id"),
+        repeats: a.hasOne("TaskRepeat", "owningTaskId"),
         description: a.string(),
-        list: a.belongsTo("TaskList", "id"),
+        taskListId: a.id(),
+        list: a.belongsTo("TaskList", "taskListId"),
     }),
     TaskRepeat: a.model({
         id: a.id().required(),
         repeatType: a.string(),
         repeatOn: a.boolean().array(),
-        task: a.belongsTo("Task", "id"),
+        owningTaskId: a.id(),
+        task: a.belongsTo("Task", "owningTaskId"),
     }),
+}
+
+function oneToOneRelationship(from, to, via) {
+
+}
+
+const kitchenModels = {
     Kitchen: a.model({
         id: a.id().required(),
-        groceries: a.hasOne("Groceries", "id"),
-        pantry: a.hasOne("Pantry", "id"),
-        household: a.belongsTo("Household", "id"),
+        groceries: a.hasOne("Groceries", "kitchenId"),
+        pantry: a.hasOne("Pantry", "kitchenId"),
+        householdId: a.id(),
+        household: a.belongsTo("Household", "householdId"),
     }),
     Groceries: a.model({
         id: a.id().required(),
-        lists: a.hasMany("GroceryList", "id"),
-        kitchen: a.belongsTo("Kitchen", "id"),
+        lists: a.hasMany("GroceryList", "groceriesId"),
+        kitchenId: a.id(),
+        kitchen: a.belongsTo("Kitchen", "kitchenId"),
     }),
     GroceryList: a.model({
         id: a.id().required(),
-        items: a.hasMany("GroceryItem", "id"),
-        groceries: a.belongsTo("Groceries", "id"),
+        items: a.hasMany("GroceryItem", "groceryListId"),
+        groceriesId: a.id(),
+        groceries: a.belongsTo("Groceries", "groceriesId"),
     }),
     GroceryItem: a.model({
         id: a.id().required(),
         name: a.string(),
         quantity: a.integer(),
         unit: a.string(),
-        list: a.belongsTo("GroceryList", "id"),
+        groceryListId: a.id(),
+        list: a.belongsTo("GroceryList", "groceryListId"),
     }),
     Pantry: a.model({
         id: a.id().required(),
         locations: a.string().array(),
-        items: a.hasMany("PantryItem", "id"),
-        kitchen: a.belongsTo("Kitchen", "id"),
+        items: a.hasMany("PantryItem", "pantryId"),
+        kitchenId: a.id(),
+        kitchen: a.belongsTo("Kitchen", "kitchenId"),
     }),
     PantryItem: a.model({
         id: a.id().required(),
@@ -69,34 +91,57 @@ const schema = a.schema({
         quantity: a.integer(),
         location: a.string(),
         expiration: a.date(),
-        pantry: a.belongsTo("Pantry", "id"),
+        pantryId: a.id(),
+        pantry: a.belongsTo("Pantry", "pantryId"),
     }),
+}
+
+const recipeModels = {
     HouseholdRecipes: a.model({
         id: a.id().required(),
-        recipes: a.hasMany("Recipe", "id"),
-        household: a.belongsTo("Household", "id"),
+        recipes: a.hasMany("Recipe", "householdRecipesId"),
+        householdId: a.id(),
+        household: a.belongsTo("Household", "householdId"),
     }),
     Recipe: a.model({
         id: a.id().required(),
         title: a.string(),
         description: a.string(),
-        ingredients: a.hasMany("RecipeIngredient", "id"),
+        ingredients: a.hasMany("RecipeIngredient", "recipeId"),
         instructions: a.string().array(),
-        belongsTo: a.belongsTo("HouseholdRecipes", "id"),
+        householdRecipesId: a.id(),
+        belongsTo: a.belongsTo("HouseholdRecipes", "householdRecipesId"),
     }),
     RecipeIngredient: a.model({
         id: a.id().required(),
-        recipe: a.belongsTo("Recipe", "id"),
-        ingredient: a.belongsTo("Ingredient", "id"),
+        recipeId: a.id(),
+        recipe: a.belongsTo("Recipe", "recipeId"),
+        ingredientId: a.id(),
+        ingredient: a.belongsTo("Ingredient", "ingredientId"),
         quantity: a.integer(),
         unit: a.string()
     }),
     Ingredient: a.model({
         id: a.id().required(),
         name: a.string(),
-        recipes: a.hasMany("RecipeIngredient", "id")
-    })
-}).authorization(allow => allow.owner().to(["read", "create", "update"]));
+        recipes: a.hasMany("RecipeIngredient", "ingredientId")
+    }),
+}
+
+// TODO: Don't want client to be able to create new households, have a backend
+//  function for it that checks if a user already has one.
+const schema = a.schema({
+    Household: a.model(ownedModel({
+        id: a.id().required(),
+        name: a.string().required(),
+        kitchen: a.hasOne("Kitchen", "householdId"),
+        householdTasks: a.hasOne("HouseholdTasks", "householdId"),
+        recipes: a.hasOne("HouseholdRecipes", "householdId")
+    })),
+    ...tasksModels,
+    ...kitchenModels,
+    ...recipeModels
+}).authorization(defaultOperations);
 
 export type Schema = ClientSchema<typeof schema>;
 
@@ -112,7 +157,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
