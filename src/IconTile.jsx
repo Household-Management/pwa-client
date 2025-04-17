805600009039dfffa329eabe5df65ac473e7fc73
ThisIsNoZaku <damienmarble@gmail.com>
2025-04-17 15:33:40 -0700

    Allow overriding properties.

--- src/IconTile.jsx
+++ src/IconTile.jsx
@@ -1,8 +1,8 @@
 import {IconButton, Paper} from "@mui/material";
 import Fab from "@mui/material/Fab";
 
-export default function IconTile({icon, onClick, color, size}) {
-    return <Paper style={{marginLeft: "32px", borderRadius: "50%", aspectRatio: 1}}>
+export default function IconTile({icon, onClick, color, size, sx}) {
+    return <Paper sx={{marginLeft: "32px", borderRadius: "50%", aspectRatio: 1, ...sx}}>
         <Fab onClick={onClick} color={color} size={size}>
             {icon}
         </Fab>
