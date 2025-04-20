a0e92985c6a325ecfe84a880e25330a9b618b8bc
ThisIsNoZaku <damienmarble@gmail.com>
2025-04-20 13:22:49 -0700

    Added hook providers.

--- src/navigation/components/HouseholdSelector.stories.jsx
+++ src/navigation/components/HouseholdSelector.stories.jsx
@@ -1,33 +1,46 @@
 import HouseholdSelectorList from './HouseholdSelectorList';
-import { fn } from "@storybook/test";
+import {fn} from "@storybook/test";
+import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+import {CookiesProvider} from "react-cookie";
+
+const store = configureStore({
+    reducer: (state, action) => {
+        return state || {}
+    }
+})
 
 const meta = {
-  component: props => <HouseholdSelectorList {...props} open={true} />,
+    component: props => <Provider store={store}>
+        <CookiesProvider>
+            <HouseholdSelectorList {...props} open={true}/>
+        </CookiesProvider>
+    </Provider>,
 };
 
 export default meta;
 
 export const Default = {
-  args: {
-    households: [
-      {
-        name: "John's Household",
-      },
-      {
-        name: "Jane's Household",
-      },
-      {
-        name: "Joe's Household",
-      }
-    ],
-    user: {
-      signInDetails: {
-        loginId: "someone@email.com"
-      }
-    },
-    loading: false,
-    onJoinHousehold: fn(),
-    onSelectHousehold: fn(),
-    onCreateHousehold: fn()
-  }
+    args: {
+        households: [
+            {
+                name: "John's Household",
+            },
+            {
+                name: "Jane's Household",
+            },
+            {
+                name: "Joe's Household",
+            }
+        ],
+        user: {
+            signInDetails: {
+                loginId: "someone@email.com"
+            }
+        },
+        loading: false,
+        onJoinHousehold: fn(),
+        onSelectHousehold: fn(),
+        onCreateHousehold: fn()
+    }
 };
