1b841fe5af4bd1668f44b8ea19763774aab2d96c
ThisIsNoZaku <damienmarble@gmail.com>
2025-04-21 19:48:33 -0700

    Default context value.

--- src/layout/hooks/HeaderContext.jsx
+++ src/layout/hooks/HeaderContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState, useContext } from "react";
 
 // Create a Context for managing header content
-const HeaderContext = createContext();
+const HeaderContext = createContext(null);
 
 // Custom hook to use the context easily in other components
 export const useHeader = () => useContext(HeaderContext);
