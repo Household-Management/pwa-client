ea628a8c9ba735e7e4f7ffa22539c17de8a42f5a
ThisIsNoZaku <damienmarble@gmail.com>
2025-09-14 15:50:51 -0700

    Stop using storybook-addon-remix-react-router, it doesn't support Storybook 9

--- src/authentication/components/SignIn.stories.tsx
+++ src/authentication/components/SignIn.stories.tsx
@@ -1,9 +1,9 @@
 import SignIn from "./SignIn";
-import {configureStore} from "@reduxjs/toolkit";
-import {Provider} from "react-redux";
-import {reactRouterParameters, withRouter} from "storybook-addon-remix-react-router";
-import {Box, Modal} from "@mui/material";
-import {fn} from "storybook/test";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { Box, Modal } from "@mui/material";
+import { MemoryRouter } from "react-router-dom";
+import { fn } from "storybook/test";
 
 const store = configureStore({
     reducer: state => state || {}
@@ -11,65 +11,54 @@ const store = configureStore({
 
 export default {
     render: args => {
-        return <Provider store={store}>
-            <Modal open={true}>
-                <div style={{
-                    boxSizing: "border-box",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                    width: "100vw"
-                }}>
-                    <Box sx={{
-                        boxSizing: "border-box",
-                        width: 400,
-                        flexShrink: 1,
-                        border: "2px solid #000",
-                        boxShadow: 24,
-                        p: 4,
-                        justifyContent: "center",
-                        alignContent: "center",
-                        display: "flex",
-                        bgcolor: "background.paper"
-                    }}>
-                        <SignIn onSubmit={async (email, password) => {
-                            args.onSubmit(email, password);
-                            return new Promise((resolve, reject) => {
-                                setTimeout(() => {
-                                    if(args.errorOnSubmit) {
-                                        reject(args.errorOnSubmit);
-                                    } else {
-                                        resolve(null);
-                                    }
-                                }, args.loadTime);
-                            })
-                        }}/>
-                    </Box>
-                </div>
-            </Modal>
-        </Provider>
-
-
-    },
-    decorators: [
-        withRouter
-    ],
-    parameters: {
-        reactRouter: reactRouterParameters({
-            location: {
-                path: "/sign-in",
-            },
-            routing: [{
-                path: "/sign-in",
-            }]
-        })
+        return (
+            <Provider store={store}>
+                <MemoryRouter initialEntries={["/sign-in"]}>
+                    <Modal open={true}>
+                        <div style={{
+                            boxSizing: "border-box",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            height: "100vh",
+                            width: "100vw"
+                        }}>
+                            <Box sx={{
+                                boxSizing: "border-box",
+                                width: 400,
+                                flexShrink: 1,
+                                border: "2px solid #000",
+                                boxShadow: 24,
+                                p: 4,
+                                justifyContent: "center",
+                                alignContent: "center",
+                                display: "flex",
+                                bgcolor: "background.paper"
+                            }}>
+                                <SignIn onSubmit={async (email, password) => {
+                                    args.onSubmit(email, password);
+                                    return new Promise((resolve, reject) => {
+                                        setTimeout(() => {
+                                            if (args.errorOnSubmit) {
+                                                reject(args.errorOnSubmit);
+                                            } else {
+                                                resolve(null);
+                                            }
+                                        }, args.loadTime);
+                                    });
+                                }} />
+                            </Box>
+                        </div>
+                    </Modal>
+                </MemoryRouter>
+            </Provider>
+        );
     },
     args: {
         loadTime: 1000,
         errorOnSubmit: null,
         onSubmit: fn()
     }
-}
+};
 
-export const SignInComponent = {}
+export const SignInComponent = {};
