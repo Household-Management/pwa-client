8ce21917e774f86d99808df90c7d4d6f337c1bc4
ThisIsNoZaku <damienmarble@gmail.com>
2025-09-17 20:17:28 -0700

    Added Realtime User Monitoring for testing.

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,35 @@ import {router} from "./navigation/configuration/routing";
 import ConfigurationService from "./config/ConfigurationService";
 import {CookiesProvider} from "react-cookie";
 import {parseAmplifyConfig} from "aws-amplify/utils";
+import {AwsRum} from 'aws-rum-web';
+if(import.meta.env.VITE_ENABLE_RUM === true) {
+    try {
+        console.log("Enabling RUM");
+        const config = {
+            sessionSampleRate: 1,
+            endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
+            telemetries: ["performance", "errors", "http"],
+            allowCookies: false,
+            enableXRay: false,
+            signing: true // If you have a public resource policy and wish to send unsigned requests please set this to false
+        };
 
-console.log("Loading state from window");
+        const APPLICATION_ID = '0eae084b-3d29-481c-a189-3b236c539304';
+        const APPLICATION_VERSION = '1.0.0';
+        const APPLICATION_REGION = 'us-east-1';
+
+        const awsRum = new AwsRum(
+            APPLICATION_ID,
+            APPLICATION_VERSION,
+            APPLICATION_REGION,
+            config
+        );
+    } catch (error) {
+        // Ignore errors thrown during CloudWatch RUM web client initialization
+    }
+} else {
+    console.log("RUM not enabled");
+}
 
 const amplifyConfiguration = parseAmplifyConfig(outputs);
 
